f5009ca40c714939e75a1601ccef3f1cf5cc9be6
Demaris Ashmore <demiashmore@yahoo.com>
2022-09-28 13:24:36 -0600

    commit tree f38323a59fbd82865900cd36d2e7d577b617cba5

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App () {
+
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [dogImage, setDogImage] = useState([])
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => {
+                setDogImage(data.message)
+                setIsLoaded(true)
+            })
+    }, [])
+
+    if(!isLoaded) return <p>Loading...</p>
+
+    return(
+        <div>
+            <img src={dogImage} alt='A Random Dog' />
+        </div>
+    )
+}
+
+export default App
